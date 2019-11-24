6fc9d6594e448a6f66391c973f30235f2e165bd4
rihab <rihab.rahali@ensi-uma.tn>
2019-11-24 10:58:51 +0100

    tunihack code

---
+++ extension/content.js
@@ -0,0 +1,32 @@
+alert("Hey buddy, you spent 70% of your time in Scientific issues  \n and 30%  in Drogs",);
+
+//chrome.tabs.getSelected(null, function(tab) {
+/*chrome.tabs.getCurrent( function(tab){
+        var tab = tab.id;
+        var tabUrl = tab.url;
+		var title = tab.title;
+		
+			
+		
+        alert(title);
+
+});*/
+/*browser.history.search({text: '', maxResults: 20}, function(data) {
+		data.forEach(function(page) {
+			alert(page.title);
+		});
+	});*/
+/*var siteToSearch = 'facebook.com';
+chrome.history.search({text: siteToSearch, callback: function(results) {
+    //Iterate through the `results` array of `HistoryItem` objects
+    //Look for lowest value of `lastVisitTime` OR call `chrome.history.getVisits` with
+    //`results[i].url` and then check the returned `VisitItem` for the `visitTime` property
+	alert(result[0]);
+}});*/
+/*chrome.extension.runtime.onInstalled.addListener(function() {
+    chrome.storage.sync.set({color: '#3aa757'}, function() {
+     alert("The color is green.");
+    });
+  });*/
+  
+ 
